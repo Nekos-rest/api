45fffb5db33a2a1208aeba768ae57027be477ab7
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-08 11:19:06 +0100

    Update random.js

--- api/random.js
+++ api/random.js
@@ -1,11 +1,12 @@
-import { createReadStream } from 'fs'
-import { pipeline } from 'stream'
 
-export default (req, res) => {
-res.setHeader('Content-Type', 'image/png')
-const imageStream = createReadStream(`${__dirname}/Nekos/${file}.png`)
+import fs from 'fs'
+import path from 'path'
+
 const file = Math.floor(1 + Math.random() * 75);
-pipeline(imageStream, res, (error) => {
-if (error) console.error(error)
-  })
+const filePath = path.resolve('.', `Nekos/${file}.jpg`)
+const imageBuffer = fs.readFileSync(filePath)
+
+export default function(req, res) {
+  res.setHeader('Content-Type', 'image/png')
+  res.send(imageBuffer)
 }
