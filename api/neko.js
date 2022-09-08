437fa9848ceb16ff44346c4d49b0690eb059e0bd
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-08 09:46:09 +0100

    Update neko.js

--- api/neko.js
+++ api/neko.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-let output = res.status(200).send(nekos[Math.floor(Math.random() * nekos.length)])
+let output = nekos[Math.floor(Math.random() * nekos.length)];
 
 res.status(200).send({
 status: "200",
