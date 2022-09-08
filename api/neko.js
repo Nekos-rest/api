6775ae59fb6ec111763f748ba4d0d0b8aa40ff44
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-08 10:35:30 +0100

    Update neko.js

--- api/neko.js
+++ api/neko.js
@@ -1,8 +1,8 @@
-export default function handler(req, res) {
 
+export default function handler(req, res) {
 let output = res.status(200).send(nekos[Math.floor(Math.random() * nekos.length)])
 res.status(200).send({
-output: output
+output
 });
 
 }
