8644f5c6b6934df12b25299a35ff0dbc4e4214db
SnoozedsGithub <88577069+Snoozeds@users.noreply.github.com>
2022-09-08 10:15:26 +0100

    Update random.js

--- api/random.js
+++ api/random.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
 const file = Math.floor(1 + Math.random() * 75);
 
-res.sendFile(`/public/Assets/Neko/${file}.png`, { root: "./" });
+res.sendFile(`/public/Assets/Nekos/${file}.png`, { root: "./" });
 
 }
